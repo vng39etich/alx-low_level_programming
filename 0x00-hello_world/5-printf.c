2284a23293e16062d0678cd1ccba9c5d9e330b5e
vng39etich <vng39etich@gmail.com>
2022-04-07 19:41:08 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print the value
+ * Return: returns 0 as the outcomes
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
