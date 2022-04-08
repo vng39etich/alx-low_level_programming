de2fba88b8067d1f15b1d24a1b9e753b7e610545
vng39etich <vng39etich@gmail.com>
2022-04-08 21:37:31 +0000

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,13 @@ int main(void)
 
 {
 		int num;
-		char letter;
+		char alp;
 
 		for (num = 0; num < 10; num++)
 			putchar((num % 10) + '0');
 
-		for (letter = 'a'; letter <= 'f'; letter++)
-			putchar(letter);
+		for (alp = 'a'; alp <= 'f'; alp++)
+			putchar(alp);
 
 		putchar('\n');
 
