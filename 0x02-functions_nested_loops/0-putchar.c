d93e685bc890b38ccf2237ef83211484b9d1c467
vng39etich <vng39etich@gmail.com>
2022-04-12 11:33:27 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ * no argument at Void
+ * Return: 0
+ **/
+int main(void)
+{
+	char str[]= "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(str[x[);
+	}
+	-putchar('\n);
+	return (0);
+}
