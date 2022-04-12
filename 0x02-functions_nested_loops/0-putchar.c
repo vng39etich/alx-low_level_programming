3b3338c48bab63ceb60c88dc296fa3ed0e16c2d7
vng39etich <vng39etich@gmail.com>
2022-04-12 13:12:27 +0000

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
- * main - entry point
- * no argument at Void
- * Return: 0
- **/
+ * main - entry pt
+ * Return: prints the output
+ */
 int main(void)
 {
-	char str[]= "_putchar";
-	int x;
 
-	for (x = 0; x < 8; x++)
-	{
-		_putchar(str[x[);
-	}
-	-putchar('\n);
-	return (0);
+	        char ermias[] = "_putchar";
+
+	        int c;
+
+	        for (c = 0; c < 8; c++)
+	        {
+	                _putchar(ermias[c]);
+	        }
+
+	        _putchar('\n');
+
+	        return (0);
 }
