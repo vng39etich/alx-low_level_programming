a63ac1a2bbc86bbbfd1f7ed2956d793af1302f2b
vng39etich <vng39etich@gmail.com>
2022-04-14 10:18:53 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * 0-isupper.c - evaluate if letter is uppercase
+ *  @c: print int
+ *  Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
