a22114ee0f70b121a3f858d93142545ddfddd20d
vng39etich <vng39etich@gmail.com>
2022-04-14 21:13:47 +0000

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ *@n: The character to print
+ */
+void print_line(int n)
+{
+		int line;
+
+		if (n > 0)
+		{
+			for (line = 0; line < n; line++)
+				_putchar('_');
+		}
+		_putchar('\n');
+}
