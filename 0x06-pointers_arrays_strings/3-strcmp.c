9ec2df16b55664c948dad57411f4ad19109a555a
vng39etich <vng39etich@gmail.com>
2022-04-24 00:10:41 +0300

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,6 @@ int _strcmp(char *s1, char *s2)
 {
 	int a = 0, b = 0, c = 0, r = 0, lim;
 
-
 	while (s1[a])
 	{
 		a++;
@@ -42,4 +41,10 @@ int _strcmp(char *s1, char *s2)
 		{
 			r = s1[c] - s2[c];
 			break;
+		}
+
+		c++;
 	}
+
+	return (r);
+}
