7ec5467f2a3befaf1c2557dc9b64593c63ebe3a1
vng39etich <vng39etich@gmail.com>
2022-04-26 16:09:47 +0300

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strspn - prints characters of string1 found in string2.
+ * @s: string source
+ * @accept: string identification
+ *
+ * Return: new string.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (y = 0; *(s + y); y++)
+	{
+		for (x = 0; *(accept + x); x++)
+		{
+			if (*(s + y) == *(accept + x))
+				break;
+		}
+	if (*(accept + x) == '\0')
+		break;
+	}
+	return (y);
+}
