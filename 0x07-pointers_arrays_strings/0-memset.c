f211eda27b7dd682f1c1be811b2dedbe4be2a596
vng39etich <vng39etich@gmail.com>
2022-04-26 14:08:45 +0300

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fill the memory with a byte
+ * @s: memory to fill
+ * @b: constant to fill
+ * @n: memory bytes to fill
+ *
+ * return: filled memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b
+	}
+
+}
