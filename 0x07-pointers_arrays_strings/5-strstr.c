f9420134c6357c29d490ba00474d1246e9aef645
vng39etich <44949831+vng39etich@users.noreply.github.com>
2022-04-26 22:25:14 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,9 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * _strstr - prints the consecutive caracters of s1 that are in s2.
  * @haystack: source string
  * @needle: searching string
+ *
  * Return: new string.
  */
 char *_strstr(char *haystack, char *needle)
@@ -24,6 +25,7 @@ char *_strstr(char *haystack, char *needle)
  * coincidence - define if the string b is inside a.
  * @a: source string
  * @b: string to be searched
+ *
  * Return: 1 if there is coincidence, otherwise 0.
  */
 int coincidence(char *a, char *b)
