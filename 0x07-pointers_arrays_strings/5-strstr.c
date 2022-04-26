2dc8ccea90e07289b5cd8e7eabeb3eb04d11cf4d
vng39etich <44949831+vng39etich@users.noreply.github.com>
2022-04-26 22:51:58 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,7 +3,6 @@
  * _strstr - prints the consecutive caracters of s1 that are in s2.
  * @haystack: source string
  * @needle: searching string
- *
  * Return: new string.
  */
 char *_strstr(char *haystack, char *needle)
