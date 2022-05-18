48abfa95493f5899a85613f11f297754cfc204fe
vng39etich <vng39etich@gmail.com>
2022-05-18 20:14:41 +0300

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
+#include "variadic_functions.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 /**
  * print_int - prints an int
  * @args: the list of args
